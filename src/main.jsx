d2d72d56d51cae853b1d4606355f1a4f3791d2b2
VictorKachin <victor.kachin@yandex.ru>
2024-06-24 20:58:16 +0300

    start project

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const rootElement = document.getElementById('root')
+const reactRoot = ReactDOM.createRoot(rootElement)
+
+reactRoot.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
+
+//https://www.youtube.com/watch?v=CGJp7PMBF1I
+//17:05
